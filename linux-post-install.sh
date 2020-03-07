bfe9bc192a810ff5740f0b560d51a6870f9e18e4
Vitalii Volkov <vtlvolkov@gmail.com>
2020-03-07 18:03:24 +0200

    changed ms prod.list to ubuntu

--- linux-post-install.sh
+++ linux-post-install.sh
@@ -94,7 +94,7 @@ function skype_install() {
 function dotnetcore_install() {
   wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
   sudo mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
-  wget -q https://packages.microsoft.com/config/debian/10/prod.list
+  wget -q https://packages.microsoft.com/config/ubuntu/19.10/prod.list
   sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
   sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
   sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
